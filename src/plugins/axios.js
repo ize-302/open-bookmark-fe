b6de397a6a0a107325d6ce46a5d851bf32fa6c4a
ozorku <adavizeozorku@gmail.com>
2022-01-06 15:26:24 +0100

    cleanup axios plugin

--- src/plugins/axios.js
+++ src/plugins/axios.js
@@ -1,14 +1,8 @@
 import axios from "axios";
-let httpSkip404, httpCancelRequest, httpSkip500, httpSilentError;
 
 export default {
   install(Vue) {
     axios.defaults.baseURL = `${process.env.VUE_APP_API_URL}/api/${process.env.VUE_APP_API_VERSION}`;
-
     Vue.prototype.$http = axios;
-    Vue.prototype.$httpSkip404 = httpSkip404;
-    Vue.prototype.$httpSkip500 = httpSkip500;
-    Vue.prototype.$httpSilentError = httpSilentError;
-    Vue.prototype.$httpCancelRequest = httpCancelRequest;
   },
 };
