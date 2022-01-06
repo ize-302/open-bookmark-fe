eefb7ef2d0b28aad760a91873c7c6cd65b8e0176
ozorku <adavizeozorku@gmail.com>
2022-01-06 23:39:25 +0100

    darkmode functionality placeholder

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,19 @@
   <c-theme-provider>
     <c-reset />
     <c-box maxW="800px" margin="0 auto" h="100vh" py="50px">
+      <!-- <c-button
+        @click="chakraToggleColorMode"
+        position="fixed"
+        bottom="20px"
+        right="20px"
+        zIndex="1"
+        :bg="chakraColorMode === 'dark' ? 'whiteAlpha.400' : 'blackAlpha.800'"
+      >
+        <c-icon
+          :name="chakraColorMode === 'dark' ? 'moon' : 'sun'"
+          :color="chakraColorMode === 'dark' ? 'blackAlpha.800' : 'orange'"
+        />
+      </c-button> -->
       <router-view />
     </c-box>
   </c-theme-provider>
@@ -10,5 +23,11 @@
 <script>
 export default {
   name: "App",
+  // inject: ["$chakraColorMode", "$chakraToggleColorMode"],
+  computed: {
+    // colorMode() {
+    //   return this.$chakraColorMode();
+    // },
+  },
 };
 </script>
--- src/custom-theme.js
+++ src/custom-theme.js
@@ -1,3 +1,5 @@
+// import { mode } from "@chakra-ui/vue";
+
 export default {
   breakpoints: ["30em", "48em", "62em", "80em"],
   fonts: {
@@ -11,5 +13,10 @@ export default {
       green: "#58C184",
       lightGreen: "rgba(89,193, 192, 5%)",
     },
+  },
+  baseStyles: {
+    CBox: () => ({
+      // bg: mode("blackAlpha.700", "whiteAlpha.400"),
+    }),
   },
 };
