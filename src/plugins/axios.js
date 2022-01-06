f52d13a02b1edf3fc8d43b031dfcf0fbf5912781
ozorku <adavizeozorku@gmail.com>
2022-01-06 15:45:03 +0100

    cleanup axios calls

--- src/plugins/axios.js
+++ src/plugins/axios.js
@@ -3,6 +3,6 @@ import axios from "axios";
 export default {
   install(Vue) {
     axios.defaults.baseURL = `${process.env.VUE_APP_API_URL}/api/${process.env.VUE_APP_API_VERSION}`;
-    Vue.prototype.$http = axios;
+    Vue.http = axios;
   },
 };
--- src/services/bookmarks.js
+++ src/services/bookmarks.js
@@ -4,17 +4,15 @@ const ENDPOINT = `/bookmarks`;
 
 export default {
   getByID(id) {
-    return Vue.prototype.$http
-      .get(`${ENDPOINT}/${id}`)
-      .then((response) => response.data);
+    return Vue.http.get(`${ENDPOINT}/${id}`).then((response) => response.data);
   },
   bookmarks() {
-    return Vue.prototype.$http
+    return Vue.http
       .get(`${ENDPOINT}?q=&page=1&per_page=10`)
       .then((response) => response.data);
   },
   create(title, url, description, isPrivate) {
-    return Vue.prototype.$http
+    return Vue.http
       .post(`${ENDPOINT}/create`, {
         title,
         url,
@@ -24,7 +22,7 @@ export default {
       .then((response) => response.data);
   },
   delete(id) {
-    return Vue.prototype.$http
+    return Vue.http
       .delete(`${ENDPOINT}/${id}`)
       .then((response) => response.data);
   },
