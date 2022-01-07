343f40036fe23a5c2da3909a937f5e9bbbd97ce3
ozorku <adavizeozorku@gmail.com>
2022-01-07 16:05:05 +0100

    add service to get trashed bookmarks

--- src/services/trash.js
+++ src/services/trash.js
@@ -3,6 +3,11 @@ import Vue from "vue";
 const ENDPOINT = `/trash`;
 
 export default {
+  fetchTrashedBookmakrs({ page, per_page }) {
+    return Vue.http
+      .get(`${ENDPOINT}?q=&page=${page}&per_page=${per_page}`)
+      .then((response) => response.data);
+  },
   trashBookmark(id) {
     return Vue.http
       .patch(`${ENDPOINT}/${id}/trash`)
