65ea7f948cf66554e9d3794062ef277a3910cace
ozorku <adavizeozorku@gmail.com>
2022-01-09 15:21:59 +0100

    refactor

--- src/services/bookmarks.js
+++ src/services/bookmarks.js
@@ -1,4 +1,10 @@
 import Vue from "vue";
+import { supabase } from "../lib/supabase";
+const session = supabase.auth.session();
+const headers = {
+  "Content-Type": "application/json",
+  Authorization: `Bearer ${session?.access_token}`,
+};
 
 const ENDPOINT = `/bookmarks`;
 
@@ -8,33 +14,49 @@ export default {
   },
   getAllBookmarks({ page, per_page }) {
     return Vue.http
-      .get(`${ENDPOINT}?q=&page=${page}&per_page=${per_page}`)
+      .get(`${ENDPOINT}?q=&page=${page}&per_page=${per_page}`, {
+        headers,
+      })
       .then((response) => response.data);
   },
   createBookmark({ title, url, comment, isPrivate }) {
     return Vue.http
-      .post(`${ENDPOINT}/create`, {
-        title,
-        url,
-        isPrivate,
-        comment,
-      })
+      .post(
+        `${ENDPOINT}/create`,
+        {
+          title,
+          url,
+          isPrivate,
+          comment,
+        },
+        {
+          headers,
+        }
+      )
       .then((response) => response.data);
   },
   deleteBookmark(id) {
     return Vue.http
-      .delete(`${ENDPOINT}/${id}/delete`)
+      .delete(`${ENDPOINT}/${id}/delete`, {
+        headers,
+      })
       .then((response) => response.data);
   },
 
   updateBookmark({ id, title, url, comment, isPrivate }) {
     return Vue.http
-      .patch(`${ENDPOINT}/${id}/update`, {
-        title,
-        url,
-        isPrivate,
-        comment,
-      })
+      .patch(
+        `${ENDPOINT}/${id}/update`,
+        {
+          title,
+          url,
+          isPrivate,
+          comment,
+        },
+        {
+          headers,
+        }
+      )
       .then((response) => response.data);
   },
 };
