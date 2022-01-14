36e2fcafc9eb7c63516f8b696b29743dbf660c21
ozorku <adavizeozorku@gmail.com>
2022-01-14 19:41:44 +0100

    add console for debugging

--- src/views/Landing.vue
+++ src/views/Landing.vue
@@ -11,16 +11,22 @@
 
 <script>
 import GoogleSignin from "../components/GoogleSignin.vue";
-import { saveTokenInCookies } from "@/utils/cookies";
+import {
+  saveTokenInCookies,
+  getTokenFromCookies,
+  verifyToken,
+} from "@/utils/cookies";
 import { supabase } from "../lib/supabase";
 
 export default {
   components: { GoogleSignin },
   name: "landing",
   mounted() {
+    console.log("env", process.env.VUE_APP_PUBLIC_SUPBASE_JWT_SECRET);
     supabase.auth.onAuthStateChange((event, session) => {
+      console.log("session", verifyToken(session));
       saveTokenInCookies(session?.access_token);
-      location.href = "/my-bookmarks";
+      if (getTokenFromCookies()) location.href = "/my-bookmarks";
     });
   },
 };
