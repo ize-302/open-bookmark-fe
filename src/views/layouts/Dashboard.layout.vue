95720d02c9cfee6a4341d91780a9f52993733c35
ozorku <adavizeozorku@gmail.com>
2022-01-15 08:40:25 +0100

    add trash menu item

--- src/views/layouts/Dashboard.layout.vue
+++ src/views/layouts/Dashboard.layout.vue
@@ -67,6 +67,10 @@ export default {
           name: "Browse",
           path: "/browse",
         },
+        {
+          name: "Trash",
+          path: "/trash",
+        },
       ],
     };
   },
